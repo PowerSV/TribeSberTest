81a32701c69a060081e1b13e52113f6d7b746749
ballo <ballo.aleksej@gmail.com>
2024-03-15 13:24:01 +0300

    fix: Pom's files

--- tribe-task-dto/src/main/java/com/tribe/task/dto/FactorialRequest.java
+++ tribe-task-dto/src/main/java/com/tribe/task/dto/FactorialRequest.java
@@ -1,5 +1,6 @@
 package com.tribe.task.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -9,6 +10,7 @@ public class FactorialRequest {
     @NotNull(message = "Некорректные входные данные: число не должно быть null")
     @PositiveOrZero(message = "Некорректные входные данные: число не должно быть отрицательным")
     @Max(value = 100, message = "Некорректные входные данные: число не должно превышать 100")
+    @JsonProperty("factorial_num")
     private int factorialNum;
 
     public int getFactorialNum() {
