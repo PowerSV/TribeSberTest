3a33d4be44ec3f56ca7fc4a4ed9bb745bb8b0e94
ballo <ballo.aleksej@gmail.com>
2024-03-16 14:12:14 +0300

    дописал тесты

--- tribe-task-app/src/test/java/com/tribe/task/controllers/FactorialControllerTests.java
+++ tribe-task-app/src/test/java/com/tribe/task/controllers/FactorialControllerTests.java
@@ -5,32 +5,30 @@ import com.tribe.task.dto.FactorialRequest;
 import com.tribe.task.dto.FactorialResponse;
 import com.tribe.task.services.FactorialCalculator;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigInteger;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(FactorialController.class)
 public class FactorialControllerTests {
-
-    @Autowired
-    private MockMvc mockMvc;
 
     @MockBean
     private FactorialCalculator service;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -38,15 +36,58 @@ public class FactorialControllerTests {
     @Test
     public void FactorialController_GetFactorialRequest() throws Exception {
         FactorialRequest request = new FactorialRequest(5);
+        String requestJson = objectMapper.writeValueAsString(request);
         FactorialResponse expected = new FactorialResponse(BigInteger.valueOf(120));
+        String expectedJson = objectMapper.writeValueAsString(expected);
 
-        given(service.calculateFactorial(any())).willReturn(expected);
+        given(service.calculateFactorial(any(FactorialRequest.class))).willReturn(expected);
+
+        mockMvc.perform(get("/factorial")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
+        verify(service, times(1)).calculateFactorial(any());
+    }
+
+    @Test
+    public void FactorialController_InvalidRequest_IllegalData() throws Exception {
+        FactorialRequest request = new FactorialRequest(-5);
 
         mockMvc.perform(get("/factorial")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
 
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(expected)));
+                .andExpect(status().isBadRequest());
+
+        verify(service, times(0)).calculateFactorial(any());
+    }
+
+    @Test
+    public void FactorialController_InvalidRequest_DataGreaterThan100() throws Exception {
+        FactorialRequest request = new FactorialRequest(101);
+
+        mockMvc.perform(get("/factorial")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+
+                .andExpect(status().isBadRequest());
+
+        verify(service, times(0)).calculateFactorial(any());
+    }
+
+    @Test
+    public void FactorialController_InvalidRequest_NullData() throws Exception {
+        FactorialRequest request = new FactorialRequest(null);
+
+        mockMvc.perform(get("/factorial")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+
+                .andExpect(status().isBadRequest());
+
+        verify(service, times(0)).calculateFactorial(any());
     }
 }
+
